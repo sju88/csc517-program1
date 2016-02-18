ef1a7dcf8c2493b14cf769161cccbebea0ef3a93
sju88 <sju2@ncsu.edu>
2016-02-18 03:34:56 +0000

    Finish admin  signup Song Ju

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,28 @@
 class AdminsController < ApplicationController
-  def new
+  
+  def show
+    @admin = Admin.find(params[:id])
   end
+  
+  def new
+    @admin = Admin.new
+  end
+  
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      flash[:success] = "Welcome to the Class Portal!"
+      redirect_to @admin
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def admin_params
+      params.require(:admin).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+    
 end
--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,8 @@
 module AdminsHelper
+    # 返回指定用户的 Gravatar
+  def gravatar_for(admin)
+    gravatar_id = Digest::MD5::hexdigest(admin.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: admin.name, class: "gravatar")
+  end
 end
