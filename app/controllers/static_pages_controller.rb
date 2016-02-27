3cfbb8630d1abae09437bc36eee3b5c8fdc446b6
sju88 <sju2@ncsu.edu>
2016-02-27 03:03:50 +0000

    fix bug final

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      redirect_to current_admin
+    elsif student_logged_in?
+      redirect_to current_student
+    elsif instructor_logged_in?
+      redirect_to current_instructor
+    end
   end
 
   def help
