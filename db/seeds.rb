faefbc3c0aea66260f70483831135526109cae18
sju88 <sju2@ncsu.edu>
2016-02-27 00:56:47 +0000

    Finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,3 @@
-Admin.create!(name:  "Super Admin",
-             email: "super@ncsu.edu",
-             password:              "123456",
-             password_confirmation: "123456",
-             preconfigured: true)
+Admin.create!(name:  "Super Admin", email: "superadmin@ncsu.edu", password: "123456", password_confirmation: "123456", preconfigured: true)
 
-20.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@ncsu.edu"
-  password = "password"
-  Admin.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+Admin.create!(name:  "Admin1", email: "admin1@ncsu.edu", password: "123456", password_confirmation: "123456" )
