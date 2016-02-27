f6cd597aeb167533f5cfd48fe927bc9298993c53
sju88 <sju2@ncsu.edu>
2016-02-27 01:01:33 +0000

    Finis

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,18 @@
 Admin.create!(name:  "Super Admin", email: "superadmin@ncsu.edu", password: "123456", password_confirmation: "123456", preconfigured: true)
 
-Admin.create!(name:  "Admin1", email: "admin1@ncsu.edu", password: "123456", password_confirmation: "123456" )
+Admin.create!(name:  "admin1", email: "admin1@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Admin.create!(name:  "admin2", email: "admin2@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+
+Student.create!(name:  "student1", email: "student1@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Student.create!(name:  "student2", email: "student2@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Student.create!(name:  "student3", email: "student3@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Instructor.create!(name:  "instructor1", email: "instructor1@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Instructor.create!(name:  "instructor2", email: "instructor3@ncsu.edu", password: "123456", password_confirmation: "123456" )
+
+Instructor.create!(name:  "instructor3", email: "instructor3@ncsu.edu", password: "123456", password_confirmation: "123456" )
