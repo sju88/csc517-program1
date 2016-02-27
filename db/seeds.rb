08ae73f51d0fd4aa477e6787116a6921ab026853
sju88 <sju2@ncsu.edu>
2016-02-27 01:24:14 +0000

    f

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ Student.create!(name:  "student3", email: "student3@ncsu.edu", password: "123456
 
 Instructor.create!(name:  "instructor1", email: "instructor1@ncsu.edu", password: "123456", password_confirmation: "123456" )
 
-Instructor.create!(name:  "instructor2", email: "instructor3@ncsu.edu", password: "123456", password_confirmation: "123456" )
+Instructor.create!(name:  "instructor2", email: "instructor2@ncsu.edu", password: "123456", password_confirmation: "123456" )
 
 Instructor.create!(name:  "instructor3", email: "instructor3@ncsu.edu", password: "123456", password_confirmation: "123456" )
